a63c7374c7686f366bd2acb08d2fa9d5771404c4
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:49 +0500

    feat(Context): add

---
+++ packages/core/context/README.md
@@ -0,0 +1,17 @@
+# @gist-ui/context
+
+
+
+## Installation
+
+```bash
+pnpm add @gist-ui/context
+# or
+yarn add @gist-ui/context
+# or
+npm i @gist-ui/context
+```
+
+## Licence
+
+This project is licensed under **MIT license**
---
+++ packages/core/context/src/index.tsx
@@ -0,0 +1,31 @@
+import { createContext, useContext as _useContext } from "react";
+import { GistUiError } from "@gist-ui/error";
+
+export const createContextScope = <ContextValue extends object>(
+  rootName: string,
+  defaultContext?: ContextValue,
+) => {
+  const Context = createContext(defaultContext);
+
+  const Provider = (props: ContextValue & { children?: React.ReactNode }) => {
+    const { children, ...rest } = props;
+
+    return <Context.Provider value={rest as ContextValue}>{children}</Context.Provider>;
+  };
+
+  const useContext = (consumerName: string) => {
+    const context = _useContext(Context);
+
+    if (!context)
+      throw new GistUiError(
+        "createContextScope",
+        `\`${consumerName}\` must be used within \`${rootName}\``,
+      );
+
+    return context;
+  };
+
+  Provider.displayName = `${rootName}Provider`;
+
+  return [Provider, useContext] as const;
+};
