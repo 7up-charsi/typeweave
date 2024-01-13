a5722a9d016eeafeb73fea3f6e33f8e5bb99c6ae
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:46:16 +0500

    fix: Select and Autocomplete Option styles

--- packages/core/theme/src/components/select.ts
+++ packages/core/theme/src/components/select.ts
@@ -9,7 +9,7 @@ const select = tv({
   slots: {
     listbox: 'w-[--reference-width] bg-white border overflow-y-auto rounded',
     option:
-      'h-10 flex items-center px-2 select-none truncate cursor-pointer data-[disabled=true]:opacity-50 data-[selected=true]:bg-info-200 data-[focused=true]:data-[selected=false]:bg-neutral-200 data-[focused=true]:data-[selected=true]:bg-info-300 data-[hovered=true]:data-[selected=false]:bg-neutral-200 data-[hovered=true]:data-[selected=true]:bg-info-300',
+      'h-10 flex items-center px-2 select-none truncate cursor-pointer data-[disabled=true]:opacity-50 data-[selected=true]:bg-info-200 data-[focused=true]:data-[selected=false]:bg-neutral-200 data-[focused=true]:data-[selected=true]:bg-info-300 data-[hovered=true]:data-[selected=false]:bg-neutral-200 data-[hovered=true]:data-[selected=true]:bg-info-300 text-neutral-800',
     noOptions: 'h-10 flex items-center justify-center capitalize text-neutral',
     loading: 'h-10 flex items-center justify-center capitalize text-neutral',
     endContent: 'flex items-center gap-1',
@@ -21,7 +21,7 @@ const select = tv({
     inputWrapper: '!cursor-pointer',
     group: 'relative isolate',
     groupHeader:
-      'sticky top-0 bg-white px-3 py-2 z-50 text-sm text-neutral-600',
+      'sticky top-0 bg-white px-3 py-2 z-50 text-sm font-medium text-neutral-600',
     groupItems: '',
   },
   variants: {
