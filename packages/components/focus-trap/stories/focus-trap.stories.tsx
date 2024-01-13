7b82fedf269a1db1f7638d2d674b16d98ff631de
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:37 +0500

    feat(focus-trap): add story

--- packages/components/focus-trap/stories/focus-trap.stories.tsx
+++ packages/components/focus-trap/stories/focus-trap.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@gist-ui/button";
 
 import { FocusTrap, FocusTrapProps } from "../src";
 
@@ -10,8 +11,22 @@ const meta: Meta<FocusTrapProps> = {
 
 export default meta;
 
-const Template = (args: FocusTrapProps) => <FocusTrap {...args} />;
-
 export const Default: StoryObj<FocusTrapProps> = {
-  render: Template,
+  render: (args) => {
+    return (
+      <FocusTrap
+        className="w-[500px] h-[500px] border flex flex-wrap items-center justify-center content-center gap-4"
+        {...args}
+      >
+        <span className="w-full text-center uppercase font-medium text-default">
+          focus can not escape this container
+        </span>
+        <Button>button 1</Button>
+        <Button>button 2</Button>
+        <Button>button 3</Button>
+        <Button>button 4</Button>
+        <Button>button 5</Button>
+      </FocusTrap>
+    );
+  },
 };
