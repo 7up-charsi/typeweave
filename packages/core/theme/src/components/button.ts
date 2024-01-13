6cb0e0a1f4c1584edcbb2b0ed62d88c7c9980e54
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:28 +0500

    feat(button): fix styles

--- packages/core/theme/src/components/button.ts
+++ packages/core/theme/src/components/button.ts
@@ -86,35 +86,35 @@ const button = tv({
       variant: "solid",
       color: "primary",
       class: {
-        base: "bg-primary text-primary-foreground [--rippleBg:theme(colors.primary-foreground/40%)]",
+        base: "bg-primary text-primary-foreground [--rippleBg:theme(colors.primary-foreground/30%)]",
       },
     },
     {
       variant: "solid",
       color: "secondary",
       class: {
-        base: "bg-secondary text-secondary-foreground [--rippleBg:theme(colors.secondary-foreground/40%)]",
+        base: "bg-secondary text-secondary-foreground [--rippleBg:theme(colors.secondary-foreground/30%)]",
       },
     },
     {
       variant: "solid",
       color: "success",
       class: {
-        base: "bg-success text-success-foreground [--rippleBg:theme(colors.success-foreground/20%)]",
+        base: "bg-success text-success-foreground [--rippleBg:theme(colors.success-foreground/30%)]",
       },
     },
     {
       variant: "solid",
       color: "info",
       class: {
-        base: "bg-info text-info-foreground [--rippleBg:theme(colors.info-foreground/40%)]",
+        base: "bg-info text-info-foreground [--rippleBg:theme(colors.info-foreground/30%)]",
       },
     },
     {
       variant: "solid",
       color: "warning",
       class: {
-        base: "bg-warning text-warning-foreground [--rippleBg:theme(colors.warning-foreground/20%)]",
+        base: "bg-warning text-warning-foreground [--rippleBg:theme(colors.warning-foreground/10%)]",
       },
     },
     {
@@ -122,6 +122,159 @@ const button = tv({
       color: "danger",
       class: {
         base: "bg-danger text-danger-foreground [--rippleBg:theme(colors.danger-foreground/40%)]",
+      },
+    },
+
+    // color / shadow
+    {
+      variant: "shadow",
+      color: "default",
+      class: {
+        base: "bg-default text-default-foreground [--rippleBg:theme(colors.default-foreground/20%)] shadow-default-400/100",
+      },
+    },
+    {
+      variant: "shadow",
+      color: "primary",
+      class: {
+        base: "bg-primary text-primary-foreground [--rippleBg:theme(colors.primary-foreground/30%)] shadow-primary-400/80",
+      },
+    },
+    {
+      variant: "shadow",
+      color: "secondary",
+      class: {
+        base: "bg-secondary text-secondary-foreground [--rippleBg:theme(colors.secondary-foreground/30%)] shadow-secondary-400/80",
+      },
+    },
+    {
+      variant: "shadow",
+      color: "success",
+      class: {
+        base: "bg-success text-success-foreground [--rippleBg:theme(colors.success-foreground/30%)] shadow-success-400/80",
+      },
+    },
+    {
+      variant: "shadow",
+      color: "info",
+      class: {
+        base: "bg-info text-info-foreground [--rippleBg:theme(colors.info-foreground/30%)] shadow-info-400/80",
+      },
+    },
+    {
+      variant: "shadow",
+      color: "warning",
+      class: {
+        base: "bg-warning text-warning-foreground [--rippleBg:theme(colors.warning-foreground/10%)] shadow-warning-400/80",
+      },
+    },
+    {
+      variant: "shadow",
+      color: "danger",
+      class: {
+        base: "bg-danger text-danger-foreground [--rippleBg:theme(colors.danger-foreground/40%)] shadow-danger-400/80",
+      },
+    },
+
+    // color / flat
+    {
+      variant: "flat",
+      color: "default",
+      class: {
+        base: "bg-default-200 text-default-800 [--rippleBg:theme(colors.default-800/20%)]",
+      },
+    },
+    {
+      variant: "flat",
+      color: "primary",
+      class: {
+        base: "bg-primary-100 text-primary-800 [--rippleBg:theme(colors.primary-800/20%)]",
+      },
+    },
+    {
+      variant: "flat",
+      color: "secondary",
+      class: {
+        base: "bg-secondary-100 text-secondary-800 [--rippleBg:theme(colors.secondary-800/20%)]",
+      },
+    },
+    {
+      variant: "flat",
+      color: "success",
+      class: {
+        base: "bg-success-100 text-success-800 [--rippleBg:theme(colors.success-800/20%)]",
+      },
+    },
+    {
+      variant: "flat",
+      color: "info",
+      class: {
+        base: "bg-info-100 text-info-800 [--rippleBg:theme(colors.info-800/20%)]",
+      },
+    },
+    {
+      variant: "flat",
+      color: "warning",
+      class: {
+        base: "bg-warning-100 text-warning-800 [--rippleBg:theme(colors.warning-800/20%)]",
+      },
+    },
+    {
+      variant: "flat",
+      color: "danger",
+      class: {
+        base: "bg-danger-100 text-danger-800 [--rippleBg:theme(colors.danger-800/20%)]",
+      },
+    },
+
+    // color / text
+    {
+      variant: "text",
+      color: "default",
+      class: {
+        base: "data-[hovered=true]:bg-default-200 text-default-700 [--rippleBg:theme(colors.default-700/20%)]",
+      },
+    },
+    {
+      variant: "text",
+      color: "primary",
+      class: {
+        base: "data-[hovered=true]:bg-primary-100 text-primary-700 [--rippleBg:theme(colors.primary-700/20%)]",
+      },
+    },
+    {
+      variant: "text",
+      color: "secondary",
+      class: {
+        base: "data-[hovered=true]:bg-secondary-100 text-secondary-700 [--rippleBg:theme(colors.secondary-700/20%)]",
+      },
+    },
+    {
+      variant: "text",
+      color: "success",
+      class: {
+        base: "data-[hovered=true]:bg-success-100 text-success-700 [--rippleBg:theme(colors.success-700/20%)]",
+      },
+    },
+    {
+      variant: "text",
+      color: "info",
+      class: {
+        base: "data-[hovered=true]:bg-info-100 text-info-700 [--rippleBg:theme(colors.info-700/20%)]",
+      },
+    },
+    {
+      variant: "text",
+      color: "warning",
+      class: {
+        base: "data-[hovered=true]:bg-warning-100 text-warning-700 [--rippleBg:theme(colors.warning-700/20%)]",
+      },
+    },
+    {
+      variant: "text",
+      color: "danger",
+      class: {
+        base: "data-[hovered=true]:bg-danger-100 text-danger-700 [--rippleBg:theme(colors.danger-700/20%)]",
       },
     },
 
@@ -173,159 +326,6 @@ const button = tv({
       color: "danger",
       class: {
         base: "border-danger-700 text-danger-800 [--rippleBg:theme(colors.danger-800/20%)]",
-      },
-    },
-
-    // color / flat
-    {
-      variant: "flat",
-      color: "default",
-      class: {
-        base: "bg-default-200 text-default-800 [--rippleBg:theme(colors.default-800/15%)]",
-      },
-    },
-    {
-      variant: "flat",
-      color: "primary",
-      class: {
-        base: "bg-primary-100 text-primary-800 [--rippleBg:theme(colors.primary-800/15%)]",
-      },
-    },
-    {
-      variant: "flat",
-      color: "secondary",
-      class: {
-        base: "bg-secondary-100 text-secondary-800 [--rippleBg:theme(colors.secondary-800/15%)]",
-      },
-    },
-    {
-      variant: "flat",
-      color: "success",
-      class: {
-        base: "bg-success-100 text-success-800 [--rippleBg:theme(colors.success-800/15%)]",
-      },
-    },
-    {
-      variant: "flat",
-      color: "info",
-      class: {
-        base: "bg-info-100 text-info-800 [--rippleBg:theme(colors.info-800/15%)]",
-      },
-    },
-    {
-      variant: "flat",
-      color: "warning",
-      class: {
-        base: "bg-warning-100 text-warning-800 [--rippleBg:theme(colors.warning-800/15%)]",
-      },
-    },
-    {
-      variant: "flat",
-      color: "danger",
-      class: {
-        base: "bg-danger-100 text-danger-800 [--rippleBg:theme(colors.danger-800/15%)]",
-      },
-    },
-
-    // color / shadow
-    {
-      variant: "shadow",
-      color: "default",
-      class: {
-        base: "bg-default text-default-foreground [--rippleBg:theme(colors.default-foreground/20%)] shadow-default-300",
-      },
-    },
-    {
-      variant: "shadow",
-      color: "primary",
-      class: {
-        base: "bg-primary text-primary-foreground [--rippleBg:theme(colors.primary-foreground/40%)] shadow-primary-400",
-      },
-    },
-    {
-      variant: "shadow",
-      color: "secondary",
-      class: {
-        base: "bg-secondary text-secondary-foreground [--rippleBg:theme(colors.secondary-foreground/40%)]",
-      },
-    },
-    {
-      variant: "shadow",
-      color: "success",
-      class: {
-        base: "bg-success text-success-foreground [--rippleBg:theme(colors.success-foreground/20%)]",
-      },
-    },
-    {
-      variant: "shadow",
-      color: "info",
-      class: {
-        base: "bg-info text-info-foreground [--rippleBg:theme(colors.info-foreground/40%)]",
-      },
-    },
-    {
-      variant: "shadow",
-      color: "warning",
-      class: {
-        base: "bg-warning text-warning-foreground [--rippleBg:theme(colors.warning-foreground/20%)]",
-      },
-    },
-    {
-      variant: "shadow",
-      color: "danger",
-      class: {
-        base: "bg-danger text-danger-foreground [--rippleBg:theme(colors.danger-foreground/40%)]",
-      },
-    },
-
-    // color / text
-    {
-      variant: "text",
-      color: "default",
-      class: {
-        base: "data-[hovered=true]:bg-default-200 text-default-700 [--rippleBg:theme(colors.default-700/15%)]",
-      },
-    },
-    {
-      variant: "text",
-      color: "primary",
-      class: {
-        base: "data-[hovered=true]:bg-primary-100 text-primary-700 [--rippleBg:theme(colors.primary-700/15%)]",
-      },
-    },
-    {
-      variant: "text",
-      color: "secondary",
-      class: {
-        base: "data-[hovered=true]:bg-secondary-100 text-secondary-700 [--rippleBg:theme(colors.secondary-700/15%)]",
-      },
-    },
-    {
-      variant: "text",
-      color: "success",
-      class: {
-        base: "data-[hovered=true]:bg-success-100 text-success-700 [--rippleBg:theme(colors.success-700/15%)]",
-      },
-    },
-    {
-      variant: "text",
-      color: "info",
-      class: {
-        base: "data-[hovered=true]:bg-info-100 text-info-700 [--rippleBg:theme(colors.info-700/15%)]",
-      },
-    },
-    {
-      variant: "text",
-      color: "warning",
-      class: {
-        base: "data-[hovered=true]:bg-warning-100 text-warning-700 [--rippleBg:theme(colors.warning-700/15%)]",
-      },
-    },
-    {
-      variant: "text",
-      color: "danger",
-      class: {
-        base: "data-[hovered=true]:bg-danger-100 text-danger-700 [--rippleBg:theme(colors.danger-700/15%)]",
       },
     },
 
