ee050c2daa1d104de55f0be2fe299b026104e4e4
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:58 +0500

    fix(useIsPressed): add check for left button

--- packages/hooks/use-is-pressed/src/index.ts
+++ packages/hooks/use-is-pressed/src/index.ts
@@ -3,23 +3,31 @@ import { useCallback, useState } from "react";
 export interface UseIsPressedProps {}
 
 const useIsPressed = <E extends HTMLElement>() => {
-  const [isPressed, setIsPressed] = useState(false);
+  const [isPressed, setIsPressed] = useState<false | "pointer" | "keyboard">(false);
 
-  const onPointerDown: React.PointerEventHandler<E> = useCallback(() => {
-    setIsPressed(true);
+  const onPointerDown = useCallback((e: React.PointerEvent<E>) => {
+    
+
+    if (e.button !== 0) return;
+
+    setIsPressed("pointer");
   }, []);
 
-  const onPointerUp: React.PointerEventHandler<E> = useCallback(() => {
-    setIsPressed(true);
+  const onPointerUp = useCallback((e: React.PointerEvent<E>) => {
+    
+
+    if (e.button !== 0) return;
+
+    setIsPressed(false);
   }, []);
 
-  const onKeyDown: React.KeyboardEventHandler<E> = useCallback((e) => {
+  const onKeyDown = useCallback((e: React.KeyboardEvent<E>) => {
     if (e.key !== "Enter" && e.key !== " ") return;
 
-    setIsPressed(true);
+    setIsPressed("keyboard");
   }, []);
 
-  const onKeyUp: React.KeyboardEventHandler<E> = useCallback((e) => {
+  const onKeyUp = useCallback((e: React.KeyboardEvent<E>) => {
     if (e.key !== "Enter" && e.key !== " ") return;
 
     setIsPressed(false);
