fb03e5652f5a62e3427bfb2c73600b58cc1ff164
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:51 +0500

    feat(hook): add use-size

---
+++ packages/hooks/use-size/README.md
@@ -0,0 +1,17 @@
+# @gist-ui/use-size
+
+
+
+## Installation
+
+```bash
+pnpm add @gist-ui/use-size
+# or
+yarn add @gist-ui/use-size
+# or
+npm i @gist-ui/use-size
+```
+
+## Licence
+
+This project is licensed under **MIT license**
---
+++ packages/hooks/use-size/src/index.ts
@@ -0,0 +1,39 @@
+import { useLayoutEffect, useState } from "react";
+
+const useSize = <E extends HTMLElement>(element: E | null) => {
+  const [size, setSize] = useState<{ width: number; height: number } | undefined>(undefined);
+
+  useLayoutEffect(() => {
+    if (element) {
+      setSize({
+        width: element.offsetWidth,
+        height: element.offsetHeight,
+      });
+
+      const observer = new ResizeObserver((enteies) => {
+        if (!enteies.length) return;
+
+        const entry = enteies[0];
+
+        setSize({
+          width: entry.borderBoxSize[0].inlineSize,
+          height: entry.borderBoxSize[0].blockSize,
+        });
+      });
+
+      observer.observe(element, { box: "border-box" });
+
+      return () => {
+        observer.unobserve(element);
+      };
+    } else {
+      setSize(undefined);
+    }
+  }, [element]);
+
+  return size;
+};
+
+export type UseSizeReturn = ReturnType<typeof useSize>;
+
+export { useSize };
