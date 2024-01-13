b0dd3cdd7291a1a95cea88021284b7834efecb9b
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:44 +0500

    feat(hook): add use-scroll-lock

---
+++ packages/hooks/use-scroll-lock/README.md
@@ -0,0 +1,17 @@
+# @gist-ui/use-scroll-lock
+
+
+
+## Installation
+
+```bash
+pnpm add @gist-ui/use-scroll-lock
+# or
+yarn add @gist-ui/use-scroll-lock
+# or
+npm i @gist-ui/use-scroll-lock
+```
+
+## Licence
+
+This project is licensed under **MIT license**
---
+++ packages/hooks/use-scroll-lock/src/index.ts
@@ -0,0 +1,84 @@
+import { RefObject, useEffect } from "react";
+
+type Direction = "horizontal" | "vertical" | "both";
+
+export interface UseScrollLockProps<E> {
+  /**
+   * Ref of element on which scroll will be locked
+   * @default document.body
+   */
+  ref?: RefObject<E | null>;
+  enabled?: boolean;
+  /**
+   * Which scroll dirction to lock
+   * @default both
+   */
+  direction?: Direction;
+}
+
+const useScrollLock = <E extends HTMLElement>(props: UseScrollLockProps<E> = {}) => {
+  const { ref, enabled, direction = "both" } = props;
+
+  useEffect(() => {
+    if (!enabled) return;
+
+    const isBody = !(ref && ref.current);
+    const ele = ref?.current || document.body;
+    const computedStyles = getComputedStyle(ele);
+
+    const originalPaddingRight = computedStyles.paddingRight;
+    const originalPaddingBottom = computedStyles.paddingBottom;
+
+    const stylePaddingRight = ele.style.paddingRight;
+    const stylePaddingBottom = ele.style.paddingBottom;
+    const styleOverflowY = ele.style.overflowY;
+    const styleOverflowX = ele.style.overflowX;
+
+    const verticalScrollBarWidth = isBody
+      ? innerWidth - document.documentElement.offsetWidth
+      : ele.offsetWidth - ele.clientWidth;
+
+    const horizontalScrollBarHeight = isBody
+      ? innerHeight - document.documentElement.offsetHeight
+      : ele.offsetHeight - ele.clientHeight;
+
+    const paddingRight = `${parseInt(originalPaddingRight, 10) + verticalScrollBarWidth}px`;
+    const paddingBottom = `${parseInt(originalPaddingBottom, 10) + horizontalScrollBarHeight}px`;
+
+    if (direction === "both") {
+      ele.style.overflowY = "hidden";
+      ele.style.overflowX = "hidden";
+      ele.style.paddingRight = paddingRight;
+      ele.style.paddingBottom = paddingBottom;
+    }
+    if (direction === "vertical") {
+      ele.style.overflowY = "hidden";
+      ele.style.paddingRight = paddingRight;
+    }
+    if (direction === "horizontal") {
+      ele.style.overflowX = "hidden";
+      ele.style.paddingBottom = paddingBottom;
+    }
+
+    return () => {
+      if (direction === "both") {
+        ele.style.overflowY = styleOverflowY;
+        ele.style.overflowX = styleOverflowX;
+        ele.style.paddingRight = stylePaddingRight;
+        ele.style.paddingBottom = stylePaddingBottom;
+      }
+      if (direction === "vertical") {
+        ele.style.overflowY = styleOverflowY;
+        ele.style.paddingRight = stylePaddingRight;
+      }
+      if (direction === "horizontal") {
+        ele.style.overflowX = styleOverflowX;
+        ele.style.paddingBottom = stylePaddingBottom;
+      }
+    };
+  }, [direction, enabled, ref]);
+};
+
+export type UseScrollLockReturn = ReturnType<typeof useScrollLock>;
+
+export { useScrollLock };
