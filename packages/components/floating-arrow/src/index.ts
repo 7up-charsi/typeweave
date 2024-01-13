e55d771adb26133efc2e63ac4c19870e93653d1a
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:51 +0500

    remove: floating-arrow

--- packages/components/floating-arrow/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as floatingArrow from "./floating-arrow";
-
-// export types
-export type { FloatingArrowProps } from "./floating-arrow";
-
-// export component
-export const FloatingArrow = floatingArrow.default;
