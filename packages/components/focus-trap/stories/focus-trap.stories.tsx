2bec6fbff8656fd0bef8bfe5bef78eca15dfbb65
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:45:39 +0500

    fix: stories

--- packages/components/focus-trap/stories/focus-trap.stories.tsx
+++ packages/components/focus-trap/stories/focus-trap.stories.tsx
@@ -15,15 +15,15 @@ export const Default: StoryObj<FocusTrapProps> = {
   render: (args) => {
     return (
       <FocusTrap {...args} asChild>
-        <div className="w-[500px] h-[500px] border flex flex-wrap content-center items-center gap-3 justify-center">
-          <span className="w-full text-center uppercase font-medium text-default">
+        <div className="w-[500px] h-[500px] border-2 border-default-300 rounded-md flex flex-wrap content-center items-center gap-3 justify-center">
+          <span className="w-full text-center uppercase font-medium mb-4 text-info-400">
             focus can not escape this container
           </span>
-          <Button>button 1</Button>
-          <Button>button 2</Button>
-          <Button>button 3</Button>
-          <Button>button 4</Button>
-          <Button>button 5</Button>
+          <Button variant="flat">button 1</Button>
+          <Button variant="flat">button 2</Button>
+          <Button variant="flat">button 3</Button>
+          <Button variant="flat">button 4</Button>
+          <Button variant="flat">button 5</Button>
         </div>
       </FocusTrap>
     );
