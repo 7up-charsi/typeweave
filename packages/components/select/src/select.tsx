47aec6a2186eea1c07af4c7d16f1d914ef291295
zeeshan <7up.charsi@gmail.com>
2024-01-13 19:46:19 +0500

    refactor: select

--- packages/components/select/src/select.tsx
+++ packages/components/select/src/select.tsx
@@ -5,6 +5,8 @@ import { useClickOutside } from '@gist-ui/use-click-outside';
 import { Option } from './option';
 import { Button } from '@gist-ui/button';
 import _groupby from 'lodash.groupby';
+import { GistUiError } from '@gist-ui/error';
+import { useFocusVisible } from '@react-aria/interactions';
 import {
   InputClassNames,
   SelectClassNames,
@@ -19,7 +21,6 @@ import {
   useRef,
   useState,
 } from 'react';
-import { GistUiError } from '@gist-ui/error';
 
 export type Reason = 'select' | 'clear';
 
@@ -190,6 +191,8 @@ const Select = <
       }
     },
   });
+
+  const { isFocusVisible } = useFocusVisible({ isTextInput: true });
 
   const inputRef = useRef<HTMLDivElement>(null);
   const [options, setOptions] = useState(optionsProp);
@@ -531,7 +534,12 @@ const Select = <
           }
           isDisabled={isDisabled}
           ref={inputRef}
-          onPointerDown={handleListboxOpen}
+          onFocus={() => {
+            if (!isFocusVisible) handleListboxOpen();
+          }}
+          onBlur={() => {
+            if (isFocusVisible) handleListboxClose();
+          }}
           classNames={{
             ...classNames,
             input: styles.input({ className: classNames?.input }),
@@ -539,17 +547,14 @@ const Select = <
               className: classNames?.inputWrapper,
             }),
           }}
-          inputProps={{
-            ...inputProps.inputProps,
-            onKeyDown: handleInputKeyDown,
-            'aria-expanded': open,
-            'aria-controls': lisboxId,
-            'aria-haspopup': 'listbox',
-            'aria-autocomplete': 'none',
-            role: 'combobox',
-            autoComplete: 'off',
-            readOnly: true,
-          }}
+          onKeyDown={handleInputKeyDown}
+          aria-expanded={open}
+          aria-controls={lisboxId}
+          aria-haspopup="listbox"
+          aria-autocomplete="none"
+          role="combobox"
+          autoComplete="off"
+          readOnly={true}
           startContent={
             startContent ? (
               <>
@@ -574,28 +579,26 @@ const Select = <
                     tabIndex={-1}
                     onPress={handleClearValue}
                   >
-                    <div>
-                      {clearIcon || (
-                        <svg
-                          width={18}
-                          height={18}
-                          viewBox="-2.4 -2.4 28.80 28.80"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          stroke="currentColor"
-                          transform="rotate(0)"
-                        >
-                          <g strokeWidth="0"></g>
-                          <g strokeLinecap="round" strokeLinejoin="round"></g>
-                          <g>
-                            <path
-                              d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
-                              fill="currentColor"
-                            ></path>
-                          </g>
-                        </svg>
-                      )}
-                    </div>
+                    {clearIcon || (
+                      <svg
+                        width={18}
+                        height={18}
+                        viewBox="-2.4 -2.4 28.80 28.80"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        stroke="currentColor"
+                        transform="rotate(0)"
+                      >
+                        <g strokeWidth="0"></g>
+                        <g strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g>
+                          <path
+                            d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
+                            fill="currentColor"
+                          ></path>
+                        </g>
+                      </svg>
+                    )}
                   </Button>
                 )}
 
