412e01c7b4713034014ebc872e83b7dc4f83add5
zeeshan <7up.charsi@gmail.com>
2024-02-18 04:08:33 +0500

    refactor(VisuallyHidden): make it server comp

--- packages/core/theme/src/components/menu.ts
+++ packages/core/theme/src/components/menu.ts
@@ -4,7 +4,7 @@ import { ClassNames } from '../types';
 export const menu = tv({
   slots: {
     menu: 'min-w-[150px] bg-muted-2 dark:bg-mutedDark-2 border border-muted-6 dark:border-mutedDark-6 rounded-md py-2 outline-none [--arrowFill:theme(colors.muted.9)] dark:[--arrowFill:theme(colors.mutedDark.9)]',
-    item: 'px-2 h-8 cursor-pointer select-none flex items-center gap-1 text-muted-11 dark:text-mutedDark-11 outline-none data-[disabled=true]:cursor-default data-[disabled=true]:opacity-50 data-[focused=true]:bg-muted-4 dark:data-[focused=true]:bg-mutedDark-4 relative [--rippleBg:theme(colors.muted-800/20%)] overflow-hidden',
+    item: 'px-2 h-8 cursor-pointer select-none flex items-center gap-1 text-muted-11 dark:text-mutedDark-11 outline-none data-[disabled=true]:cursor-default data-[disabled=true]:opacity-50 data-[focused=true]:bg-muted-4 dark:data-[focused=true]:bg-mutedDark-4 relative overflow-hidden',
     itemIcon:
       'w-[15px] h-full overflow-hidden flex items-center justify-center',
     itemContent: 'first-letter:uppercase',
