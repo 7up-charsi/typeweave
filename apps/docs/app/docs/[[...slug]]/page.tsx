e9c554c26941f267004314cf290123927533f16b
zeeshan <7up.charsi@gmail.com>
2024-02-21 11:15:10 +0500

    chore(docs): make better

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -1,12 +1,27 @@
 import { notFound } from 'next/navigation';
 import { readMarkdown } from '@/lib/readMarkdown';
 import grayMatter from 'gray-matter';
-import RenderMarkdown from '@/components/RenderMarkdown';
 import { PageHeader } from '@/components/PageHeader';
 import PrevNextLinks from '@/components/PrevNextLinks';
 import { componentsLinks } from '@/config/componentsLinks';
 import { getHeadings } from '@/lib/utils';
 import Toc from '@/components/Toc';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic';
+import rehypeShiki from '@shikijs/rehype';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const components: MDXRemoteProps['components'] = {
+  h1: (props) => <h2 {...props} className="scroll-mt-20" />,
+  h2: (props) => <h2 {...props} className="scroll-mt-20" />,
+  h3: (props) => <h2 {...props} className="scroll-mt-20" />,
+  h4: (props) => <h2 {...props} className="scroll-mt-20" />,
+  h5: (props) => <h2 {...props} className="scroll-mt-20" />,
+  h6: (props) => <h2 {...props} className="scroll-mt-20" />,
+};
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const activeSlug = `/${params.slug?.join('/')}`;
@@ -25,8 +40,30 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
     <div className="flex">
       <main className="grow">
         <PageHeader heading={data.title} description={data.description} />
-        <div className="prose">
-          <RenderMarkdown>{content}</RenderMarkdown>
+        <div className="prose  data-[highlighted-line]">
+          <MDXRemote
+            source={content}
+            components={components}
+            options={{
+              mdxOptions: {
+                remarkPlugins: [remarkGfm],
+                rehypePlugins: [
+                  [rehypePrettyCode, {}],
+                  [rehypeShiki, { theme: 'one-dark-pro' }],
+                  rehypeSlug,
+                  [
+                    rehypeAutolinkHeadings,
+                    {
+                      behavior: 'append',
+                      content: fromHtmlIsomorphic('<span>#</span>', {
+                        fragment: true,
+                      }),
+                    },
+                  ],
+                ],
+              },
+            }}
+          />
         </div>
         <PrevNextLinks links={componentsLinks} activeSlug={activeSlug} />
       </main>
