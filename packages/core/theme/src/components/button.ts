294626819bfd196333c7ee4c84eb355a75a15f3b
zeeshan <7up.charsi@gmail.com>
2024-02-27 10:47:52 +0500

    chore(Button): make first letter uppercase

--- packages/core/theme/src/components/button.ts
+++ packages/core/theme/src/components/button.ts
@@ -15,7 +15,7 @@ export const buttonGroup = tv({
 
 export const button = tv({
   base: [
-    'z-0 group relative inline-flex items-center justify-center box-border rounded appearance-none select-none whitespace-nowrap font-normal subpixel-antialiased overflow-hidden tap-highlight-transparent transition-colors outline-none',
+    'z-0 group relative inline-flex items-center justify-center box-border rounded appearance-none select-none whitespace-nowrap font-normal subpixel-antialiased overflow-hidden tap-highlight-transparent transition-colors outline-none [&>span]:first-letter:uppercase',
     ...dataFocusVisible,
   ],
   variants: {
