a80242e36429edacd87c9171ec214121e1af00d6
zeeshan <7up.charsi@gmail.com>
2024-03-01 16:55:29 +0500

    chore(docs): make beter

--- apps/docs/components/code-preview.tsx
+++ apps/docs/components/code-preview.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const CodePreview = ({ children }: Props) => {
   return (
-    <div className="p-4 min-h-40 max-h-60 overflow-auto flex items-center justify-center border border-muted-6 dark:border-mutedDark-6 rounded-xl bg-muted-2">
+    <div className="p-4 min-h-40 overflow-auto flex items-center justify-center border border-muted-6 dark:border-mutedDark-6 rounded-xl bg-muted-2">
       {children}
     </div>
   );
--- apps/docs/components/mdx-components.tsx
+++ apps/docs/components/mdx-components.tsx
@@ -4,6 +4,7 @@ import { DocHeader } from './doc-header';
 import { CodeBlock } from './code-block';
 import { CodePreview } from './code-preview';
 import { CodeSource } from './code-source';
+import { Highlight } from './highlight';
 import * as demos from './demos';
 
 export const mdxComponents: MDXRemoteProps['components'] = {
@@ -11,7 +12,9 @@ export const mdxComponents: MDXRemoteProps['components'] = {
   CodePreview,
   CodeSource,
   DocHeader,
+  Highlight,
   h2: (props) => <HeadingLink as="h2" {...props} />,
   h3: (props) => <HeadingLink as="h3" {...props} />,
   code: (props) => <CodeBlock {...props} />,
+  p: (props) => <p {...props} className="mt-4" />,
 };
