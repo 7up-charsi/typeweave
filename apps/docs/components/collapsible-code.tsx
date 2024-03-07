fb439d1b831b0810f71bc2b78b91a153f5857ef3
zeeshan <7up.charsi@gmail.com>
2024-03-07 22:37:12 +0500

    chore(docs): make better

--- apps/docs/components/collapsible-code.tsx
+++ apps/docs/components/collapsible-code.tsx
@@ -1,39 +1,32 @@
 'use client';
 
 import { Button } from '@webbo-ui/button';
-import { useRef, useState } from 'react';
-import { CopyCode } from './copy-code';
+import { useState } from 'react';
 
 interface Props {
   children?: React.ReactNode;
-  code?: string;
 }
 
-export const CollapsibleCodeSource = (props: Props) => {
-  const { children, code } = props;
+export const CollapsibleCode = (props: Props) => {
+  const { children } = props;
 
   const [isCollapsed, setIsCollapsed] = useState(true);
-
-  const preRef = useRef<HTMLPreElement>(null);
 
   return (
     <div className="relative isolate">
       <pre
-        ref={preRef}
-        className="language-tsx w-full h-full relative"
         style={{
-          height: isCollapsed ? 200 : 450,
+          height: isCollapsed ? 200 : 'auto',
+          maxHeight: 450,
           overflow: isCollapsed ? 'hidden' : 'auto',
         }}
       >
-        <code className="language-tsx">{children}</code>
+        {children}
 
         {isCollapsed && (
           <div className="absolute inset-0 z-40 bg-gradient-to-t from-black"></div>
         )}
       </pre>
-
-      <CopyCode code={code} />
 
       <Button
         className="absolute bottom-5 left-1/2 -translate-x-1/2 z-50"
@@ -42,7 +35,6 @@ export const CollapsibleCodeSource = (props: Props) => {
         variant="solid"
         onPress={() => {
           setIsCollapsed((prev) => !prev);
-          preRef.current?.scrollTo({ top: 0, left: 0 });
         }}
       >
         {isCollapsed ? 'expand code' : 'collapse code'}
