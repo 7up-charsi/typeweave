1b1a5e33adb8279223a87029799267a54a46d5ac
zeeshan <7up.charsi@gmail.com>
2024-03-08 21:35:52 +0500

    chore(docs): add tabs in installation comp

--- apps/docs/components/installation.tsx
+++ apps/docs/components/installation.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { Button } from '@webbo-ui/button';
+import * as Tabs from '@webbo-ui/tabs';
+import { Code } from './code';
+import { Pre } from './pre';
+import { useState } from 'react';
+import { CopyCode } from './copy-code';
+
 interface Props {
   package?: string;
 }
@@ -7,5 +14,38 @@ interface Props {
 export const Installation = (props: Props) => {
   const { package: pkg } = props;
 
-  return <div>{pkg}</div>;
+  const [selected, setSelected] = useState('pnpm');
+
+  return (
+    <Tabs.Root value={selected} onValueChange={setSelected} className="gap-0">
+      <Tabs.List className="gap-3">
+        {[{ value: 'pnpm' }, { value: 'npm' }, { value: 'yarn' }].map(
+          ({ value }, i) => (
+            <Tabs.Trigger key={i} value={value}>
+              <Button
+                color={selected === value ? 'primary' : 'default'}
+                variant={selected === value ? 'border' : 'text'}
+                size="sm"
+              >
+                {value}
+              </Button>
+            </Tabs.Trigger>
+          ),
+        )}
+      </Tabs.List>
+
+      {[
+        { value: 'pnpm', bash: `pnpm add ${pkg}` },
+        { value: 'npm', bash: `npm install ${pkg}` },
+        { value: 'yarn', bash: `yarn add ${pkg}` },
+      ].map(({ bash, value }, i) => (
+        <Tabs.Content key={i} value={value}>
+          <Pre>
+            <Code className="language-bash">{bash}</Code>
+            <CopyCode code={bash} />
+          </Pre>
+        </Tabs.Content>
+      ))}
+    </Tabs.Root>
+  );
 };
