ac77183d9f82991095fa79517a2cec5529e6d7e7
zeeshan <7up.charsi@gmail.com>
2024-03-23 09:58:46 +0500

    feat(Icon): add

---
+++ packages/components/icon/README.md
@@ -0,0 +1,17 @@
+# @webbo-ui/icon
+
+
+
+## Installation
+
+```bash
+pnpm add @webbo-ui/icon
+# or
+yarn add @webbo-ui/icon
+# or
+npm i @webbo-ui/icon
+```
+
+## Licence
+
+This project is licensed under **MIT license**
---
+++ packages/components/icon/stories/icon.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Icon } from "../src";
+
+const meta = {
+  title: "Components/Icon",
+};
+
+export default meta;
+
+const DefaultTemplate = (args) => <Icon {...args} />;
+
+export const Default = {
+  render: DefaultTemplate,
+};
