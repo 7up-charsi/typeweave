2d27f40a54772d4ecdaeede874e4c5496d474a7a
zeeshan <7up.charsi@gmail.com>
2024-05-03 19:15:40 +0500

    refactor(FocusTrap): rewrite

--- packages/react/stories/focus-trap.stories.tsx
+++ packages/react/stories/focus-trap.stories.tsx
@@ -9,23 +9,32 @@ const meta = {
 export default meta;
 
 const DefaultTemplate = (args: FocusTrapProps) => {
+  const [state, setState] = React.useState(true);
+
   return (
     <div className="flex flex-col gap-4 items-center">
       <FocusTrap {...args}>
-        <div className="w-[500px] h-[300px] border bg-muted-2 border-muted-6 rounded flex flex-wrap content-center items-center gap-3 justify-center">
+        <div className="w-[500px] h-[300px] border-2 border-primary-6 rounded flex flex-wrap content-center items-center gap-3 justify-center">
           <span className="w-full text-center font-medium mb-4 text-info11">
             Focus can not escape this container with
             <span className="uppercase text-lg mx-1">keyboard</span>
           </span>
-          <Button>button 1</Button>
+
+          {state && <Button>button 1</Button>}
           <Button>button 2</Button>
-          <Button>button 3</Button>
+          <Button onPress={() => setState(false)}>button 3</Button>
           <Button>button 4</Button>
           <Button>button 5</Button>
         </div>
       </FocusTrap>
 
-      <Button color="primary">you cannot focus me when trapped is true</Button>
+      <p className="text-info-11">
+        You cannot focus this button with keyboard <br /> when <b>`trapped`</b>{' '}
+        and <b>`loop`</b>
+        both are true
+      </p>
+
+      <Button>button 6</Button>
     </div>
   );
 };
