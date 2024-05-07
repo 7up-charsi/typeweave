a2765a19721508ec5199e4bdfa2fca5a7309e5f9
zeeshan <7up.charsi@gmail.com>
2024-05-07 06:56:49 +0500

    feat(Ment): checked icon

--- packages/theme/src/components/alert-dialog.ts
+++ packages/theme/src/components/alert-dialog.ts
@@ -4,7 +4,7 @@ import { ClassNames } from '../types';
 export const alertDialog = tv({
   slots: {
     content:
-      'fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col max-w-xs w-full outline-none rounded p-4 bg-paper shadow-modal',
+      'fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col w-[calc(100%-16px)] max-w-sm outline-none rounded p-4 bg-paper shadow-modal',
     title: 'text-lg font-semibold text-muted-11',
     description: 'mt-2 text-muted-11 text-sm',
     actions: 'flex gap-2 items-center justify-end mt-3',
--- packages/theme/src/components/dialog.ts
+++ packages/theme/src/components/dialog.ts
@@ -4,7 +4,7 @@ import { ClassNames } from '../types';
 export const dialog = tv({
   slots: {
     content:
-      'fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col bg-paper max-w-md w-full outline-none overflow-hidden rounded p-4 shadow-modal',
+      'fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col bg-paper max-w-md w-[calc(100%-16px)] outline-none overflow-hidden rounded p-4 shadow-modal',
     title: 'text-lg font-semibold text-muted-11 mb-2',
     description: 'text-normal text-muted-11',
   },
