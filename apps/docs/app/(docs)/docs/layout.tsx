19d63c03b7541f0c4b76b00a0068372e457611bf
zeeshan <7up.charsi@gmail.com>
2024-06-01 05:30:58 +0500

    fix: responsive

--- apps/docs/app/(docs)/docs/layout.tsx
+++ apps/docs/app/(docs)/docs/layout.tsx
@@ -14,10 +14,16 @@ const Layout = ({ children }: Props) => {
       <body className="m-auto max-w-screen-2xl bg-background text-foreground">
         <DocsNavbar />
 
-        <div className="lg:grid lg:grid-cols-[300px_1fr]">
-          <aside className="fixed top-16 flex h-[calc(100vh-theme(spacing.16))] w-[300px] flex-col gap-3 overflow-auto border-r border-r-muted-6 py-5 pl-10 pr-5 max-lg:hidden">
-            <SidebarContent />
-          </aside>
+        <div className="xl:grid xl:grid-cols-[300px_1fr]">
+          {/*
+          why aside in div 
+          Since the aside element is positioned absolutely, it needs to be replaced with some other content so that the grid columns remain as intended.
+          */}
+          <div className="max-xl:hidden">
+            <aside className="fixed top-16 flex h-[calc(100vh-theme(spacing.16))] w-[300px] flex-col gap-3 overflow-auto border-r border-r-muted-6 py-5 pl-10 pr-5">
+              <SidebarContent />
+            </aside>
+          </div>
 
           {children}
         </div>
