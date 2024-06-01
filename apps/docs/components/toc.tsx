216dd137c3156ee7591be12ecfe7ccfd7b147a04
zeeshan <7up.charsi@gmail.com>
2024-06-02 00:11:09 +0500

    fix(docs): Toc layout shift

--- apps/docs/components/toc.tsx
+++ apps/docs/components/toc.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Skeleton } from '@typeweave/react';
 import Link from 'next/link';
 import React from 'react';
 
@@ -14,8 +15,6 @@ export const Toc = () => {
     const headingElements: HTMLHeadingElement[] = Array.from(
       document.querySelectorAll('[data-mdx-heading]'),
     );
-
-    if (!headingElements.length) return;
 
     setHeadings(headingElements);
 
@@ -41,36 +40,59 @@ export const Toc = () => {
     return () => observer.disconnect();
   }, []);
 
-  if (!headings) return;
-
   return (
     <aside className="sticky top-16 hidden h-[calc(100vh-theme(spacing.16))] w-[200px] shrink-0 p-2 pt-4 lg:block">
       <span className="text-sm text-muted-11">Table of contents</span>
       <nav aria-label="table of content" className="flex flex-col">
-        {headings.map(({ id, innerText, dataset }, i) => {
-          const depth = dataset.depth as unknown as string;
+        {headings ? null : (
+          <div className="space-y-2 pr-5 text-sm">
+            <Skeleton variant="text" />
+            <Skeleton variant="text" />
+            <Skeleton variant="text" />
+            <Skeleton variant="text" className="ml-5 w-auto" />
+            <Skeleton variant="text" className="ml-5 w-auto" />
+            <Skeleton variant="text" className="ml-5 w-auto" />
+            <Skeleton variant="text" className="ml-5 w-auto" />
+            <Skeleton variant="text" />
+            <Skeleton variant="text" />
+          </div>
+        )}
 
-          return (
-            <div key={i} className="flex h-8 items-center gap-2">
-              <div
-                style={{ marginLeft: +depth === 2 ? 0 : +depth * 5 }}
-                className={`h-1/3 w-1 rounded-full ${activeId === id ? 'bg-primary-9' : 'bg-transparent'}`}
-              ></div>
-              <Link
-                href={`#${id}`}
-                className={`text-sm ${
-                  activeId === id
-                    ? 'text-primary-11 hover:text-primary-12'
-                    : 'text-muted-11 hover:text-muted-12'
-                }`}
-              >
-                <span className="inline-block first-letter:uppercase ">
-                  {innerText}
-                </span>
-              </Link>
-            </div>
-          );
-        })}
+        {headings && !headings.length ? (
+          <span className="mt-10 text-sm text-muted-11/70">
+            No headings found
+          </span>
+        ) : null}
+
+        {!headings?.length
+          ? null
+          : headings.map(({ id, innerText, dataset }, i) => {
+              const depth = dataset.depth as unknown as string;
+
+              return (
+                <div key={i} className="flex h-8 items-center gap-2">
+                  <div
+                    style={{
+                      marginLeft: +depth === 2 ? 0 : +depth * 5,
+                    }}
+                    className={`h-1/3 w-1 rounded-full ${activeId === id ? 'bg-primary-9' : 'bg-transparent'}`}
+                  ></div>
+
+                  <Link
+                    href={`#${id}`}
+                    className={`text-sm ${
+                      activeId === id
+                        ? 'text-primary-11 hover:text-primary-12'
+                        : 'text-muted-11 hover:text-muted-12'
+                    }`}
+                  >
+                    <span className="inline-block first-letter:uppercase ">
+                      {innerText}
+                    </span>
+                  </Link>
+                </div>
+              );
+            })}
       </nav>
     </aside>
   );
