868cea28241c7ba8217d88e3e8d04a1eaeb3f2af
zeeshan <7up.charsi@gmail.com>
2024-06-05 11:15:31 +0500

    fix(docs): landing page

--- apps/docs/app/(landing-page)/page.tsx
+++ apps/docs/app/(landing-page)/page.tsx
@@ -1,97 +1,95 @@
+import Link from 'next/link';
+
 const Page = () => {
   return (
-    <main>
-      <h1>Empower Your UI with Typeweave</h1>
+    <>
+      <main className="mt-0 flex flex-col items-center px-8 text-center lg:my-20 lg:px-14">
+        <h1 className="p-6 text-[length:max(48px,min(5vw,76px))] font-extrabold">
+          Type it safe, Style it right.
+        </h1>
 
-      <p>
-        Welcome to TypeWeave, your go-to source for high-quality and
-        W3C compliant React components. Developed to bring efficiency
-        and flexibility to your development process.
-      </p>
+        <p className="px-8 py-6 lg:px-8 lg:py-10">
+          Unleash type safety & rapid UI dev. Pre-built,{' '}
+          <strong>type-safe</strong> React components for effortless{' '}
+          <strong>TailwindCSS</strong> integration. Write cleaner
+          code, style UIs faster and ship features quicker.
+        </p>
 
-      <h2>Get Started Quickly</h2>
+        <h2 className="mt-10 text-center text-2xl">
+          Get Started Quickly
+        </h2>
 
-      <p>
-        Whether you're building a small project or a large-scale
-        application, Typeweave provides a comprehensive set of tools
-        and components to get you started quickly. Follow our
-        installation guide to begin.
-      </p>
+        <p className="mx-auto mt-10 max-w-md text-center">
+          Whether you're building a small project or a large-scale
+          application, Typeweave provides a comprehensive set of
+          components to get you started quickly. Follow our{' '}
+          <Link
+            className="italic text-info-11 underline underline-offset-2"
+            href="/guides/installation"
+          >
+            installation
+          </Link>{' '}
+          guide to begin.
+        </p>
 
-      <h2>Features</h2>
+        <h2 className="mt-10 text-center text-2xl">Features</h2>
 
-      <dl>
-        <dt>
-          Modular Design
-          <dd>
+        <dl className="mx-auto max-w-md">
+          <dt className="mb-2 font-semibold">Modular Design</dt>
+          <dd className="mb-4 pl-4">
             Typeweave’s modular design allows you to pick and choose
             the components you need, making your projects lightweight
             and customizable.
           </dd>
-        </dt>
 
-        <dt>
-          TailwindCSS
-          <dd>
+          <dt className="mb-2 font-semibold">TailwindCSS</dt>
+          <dd className="mb-4 pl-4">
             Typeweave leverage the power of TailwindCSS for styling,
             ensuring your UI components are easy to customize, and
             maintainable.
           </dd>
-        </dt>
 
-        <dt>
-          TypeScript
-          <dd>
+          <dt className="mb-2 font-semibold">TypeScript</dt>
+          <dd className="mb-4 pl-4">
             Typeweave is built with TypeScript, providing strong type
             definitions and improving your development experience with
             better autocompletion and type checking.
           </dd>
-        </dt>
 
-        <dt>
-          Accessibility
-          <dd>
+          <dt className="mb-2 font-semibold">Accessibility</dt>
+          <dd className="mb-4 pl-4">
             All Typeweave components are developed following W3C
             accessibility guidelines, ensuring that your applications
             are usable by everyone, including those with disabilities.
             This commitment to accessibility helps you create
             inclusive and compliant web applications.
           </dd>
-        </dt>
 
-        <dt>
-          Customizable Themes
-          <dd>
+          <dt className="mb-2 font-semibold">Customizable Themes</dt>
+          <dd className="mb-4 pl-4">
             Typeweave components are unstyled by default, giving you
             the freedom to style them as you see fit. Use the
             @typeweave/theme package to apply our default theme or
             bring your own theme to match your brand's identity.
           </dd>
-        </dt>
 
-        <dt>
-          Developer Friendly
-          <dd>
+          <dt className="mb-2 font-semibold">Developer Friendly</dt>
+          <dd className="mb-4 pl-4">
             Typeweave is designed with developers in mind. All
             components are easy to use and integrate seamlessly into
             your projects. With comprehensive documentation and a
             focus on simplicity, you'll be up and running in no time.
           </dd>
-        </dt>
 
-        <dl>
-          <dt>
-            Robust Components
-            <dd>
-              TypeWeave provides a variety of components to meet your
-              needs, from buttons to modals, without any unnecessary
-              extras.
-            </dd>
-          </dt>
+          <dt className="mb-2 font-semibold">Robust Components</dt>
+          <dd className="pl-4">
+            TypeWeave provides a variety of components to meet your
+            needs, from buttons to modals, without any unnecessary
+            extras.
+          </dd>
         </dl>
-      </dl>
 
-      {/* <div className="mx-auto mt-5 flex w-full max-w-[500px] flex-col items-center justify-center divide-y">
+        {/* <div className="mx-auto mt-5 flex w-full max-w-[500px] flex-col items-center justify-center divide-y">
           {['pnpm add', 'npm install', 'yarn add'].map((pm) => (
             <pre className="group relative h-12 w-full content-center overflow-auto px-3">
               <code>
@@ -105,7 +103,8 @@ const Page = () => {
             </pre>
           ))}
         </div> */}
-    </main>
+      </main>
+    </>
   );
 };
 
