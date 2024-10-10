966a404bcb55f6b4a3d11482dcb2346ac6b1e2db
zeeshan <7up.charsi@gmail.com>
2024-10-10 19:40:01 +0500

    chore: switch back to tsc

--- packages/react/tsup.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'tsup';
-import { tsupSharedConfig } from '../../shared_tsup.config';
-
-export default defineConfig({
-  ...tsupSharedConfig,
-  entry: ['!./src/**/*.stories.tsx', './src/**'],
-});
--- shared_tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { Options } from 'tsup';
-
-export const tsupSharedConfig: Options = {
-  clean: true,
-  outDir: 'dist',
-  experimentalDts: true,
-  target: ['esnext'],
-  format: 'esm',
-};
