6f6630590f36049facaa9c7cab5ca8528ecd1211
zeeshan <7up.charsi@gmail.com>
2024-10-17 09:22:22 +0500

    fix(React): alert-dialog descritpion style

--- packages/react/src/alert-dialog/alert-dialog.styles.ts
+++ packages/react/src/alert-dialog/alert-dialog.styles.ts
@@ -5,8 +5,8 @@ export const alertDialogStyles = tv({
     content:
       'fixed z-50 bg-paper outline-none overflow-hidden shadow-depth-elevation top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 max-w-sm w-full rounded p-5',
     title: 'text-lg text-muted-12 first-letter:uppercase font-semibold',
-    description: 'mt-2',
-    actions: 'flex items-center text-sm justify-end gap-2 mt-5',
+    description: 'mt-2 text-sm',
+    actions: 'flex items-center justify-end gap-2 mt-5',
   },
 });
 
